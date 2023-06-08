05e2cf4c3bdea32992021bcbf22b6093c5bb9148
grem62 <gremontmatheo22@gmail.com>
2023-06-08 13:21:44 +0200

    Write a function that prints x elements of a list.

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         count = 0
         for element in my_list:
-            print(element, end=" ")
+            print(element, end="")
             count += 1
             if count == x:
                 break
