7a75e050dd3d9cd7d1fd448e9e0b249a83c3b53b
grem62 <gremontmatheo22@gmail.com>
2023-06-14 11:11:42 +0200

    add the file for the task 0 addtion with two integer

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+module 0-add_int
+"""
+def add_integer(a, b=98):
+    """
+    add integer
+    addition two integer
+    renvoie Value
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    if isinstance(a, float) or isinstance(b, float):
+        a = int(a)
+        b = int(b)
+    return (a + b)
+    
