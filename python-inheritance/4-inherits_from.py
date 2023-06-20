35abf1785f3b4ead3d59c754e1620b342aea2b06
grem62 <gremontmatheo22@gmail.com>
2023-06-21 01:08:00 +0200

    correction code

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """fonction determine if is a instance of calss"""
 
+
 def inherits_from(obj, a_class):
     """
     Function to determine if obj is an instance of a_class
@@ -10,9 +11,8 @@ def inherits_from(obj, a_class):
         obj: object to look at
         a_class: class to verify the instance of
     """
-    
+
     if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     else:
         False
-
