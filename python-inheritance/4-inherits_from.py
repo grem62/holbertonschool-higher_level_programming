8779e84778e034c45dda612e7ea603c48fb6823d
grem62 <gremontmatheo22@gmail.com>
2023-06-21 01:06:13 +0200

    Write a function that returns True if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class ; otherwise False.

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
-
+"""fonction determine if is a instance of calss"""
 
 def inherits_from(obj, a_class):
-    return issubclass(type(obj), a_class) and type(obj) != a_class
+    """
+    Function to determine if obj is an instance of a_class
+    inherited from the specified class.
+
+    Args:
+        obj: object to look at
+        a_class: class to verify the instance of
+    """
+    
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    else:
+        False
+
