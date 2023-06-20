66c780f26d9c8279f3d57269d1d2b5ea989b90ad
grem62 <gremontmatheo22@gmail.com>
2023-06-20 15:20:55 +0200

    Write a class MyList that inherits from list:

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Définition de la classe MyList qui hérite de la classe list"""
+
+
+class MyList(list):
+
+    """Définition de la méthode print_sorted"""
+    def print_sorted(self):
+
+        """Utilisation de la fonction sorted pour trier la liste self"""
+        print(sorted(self))
