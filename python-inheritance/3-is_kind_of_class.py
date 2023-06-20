096a57ec288f2092857600b5a7f09c8e48dcf6a9
grem62 <gremontmatheo22@gmail.com>
2023-06-20 17:40:41 +0200

    correction indentation

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,22 +1,12 @@
 #!/usr/bin/pythin3
 """
-Finds if an object is an instance of a class.
+Définition de la fonction is_kind_of_class
 """
 
 
 def is_kind_of_class(obj, a_class):
-    """Function to determine if obj is an instance of a_class or
-    class inherited.
-
-    Args:
-        obj: object to look at
-        a_class: class to verify the instance of
-
-    Returns: True if obj is an instance of a_class or inherited class,
-             False otherwise
     """
-
-    if isinstance(obj, a_class):
-        return True
-    else:
-        return False
+    Vérifie si obj est une instance de a_class en utilisant isinstance
+    Retourne True si obj est une instance de a_class, sinon False
+    """
+    return True if isinstance(obj, a_class) else False
