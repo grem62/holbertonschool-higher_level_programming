3d81b029b16da8ca26686f2805f7742f8d8a71aa
grem62 <gremontmatheo22@gmail.com>
2023-06-22 13:28:18 +0200

    correction arguments

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+"""
+open a file and read the text
+"""
+
 
 def read_file(filename=""):
+    """
+    use the fonction open for be inside a file and after execute the setting
+    """
+
     with open(filename, "r", encoding='utf-8') as mon_fichier_a_moiiiiiii:
         read = mon_fichier_a_moiiiiiii.read()
         print(read)
