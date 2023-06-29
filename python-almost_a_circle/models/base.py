98a377bc1748f9c3d6ae7d38c18466b3efe957f0
grem62 <gremontmatheo22@gmail.com>
2023-06-29 17:17:29 +0200

    returns the list of the JSON string representation json_string:

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -43,3 +43,10 @@ class Base:
             json_list = [obj.to_dictionary() for obj in list_objs]
         with open(filename, "w") as file:
             file.write(cls.to_json_string(json_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None or []:
+            return []
+        else:
+            return json.loads(json_string)
