12467fff4efe8d226acdf86abf431ba1f7cf8fef
grem62 <gremontmatheo22@gmail.com>
2023-07-26 11:23:11 +0200

    corection

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
 -- script that creates the MySQL server user user_0d_1
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
--- user_0d_1 should have all privileges on the MySQL server
 GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
