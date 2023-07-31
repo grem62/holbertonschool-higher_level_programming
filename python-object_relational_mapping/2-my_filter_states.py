c18dcc528e80a5f9a9f1ca82b5f2afb256db599e
grem62 <gremontmatheo22@gmail.com>
2023-07-31 16:06:37 +0200

    correction code

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
-"""
-Write a script that takes in an argument
-and displays all values in the states table
-of hbtn_0e_0_usa where name matches the argument.
-"""
+"""Lists all states  starting with 'N' from the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host="localhost", port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3])
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * \
-                 FROM `states` \
-                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
-    [print(state) for state in c.fetchall()]
+    
+    cur = db.cursor()
+
+    cur.execute(
+            "SELECT * FROM states \
+            WHERE name = '{}' \
+            ORDER BY states.id ASC;".format(sys.argv[4]))
+
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
+
+    cur.close()
+    db.close()
