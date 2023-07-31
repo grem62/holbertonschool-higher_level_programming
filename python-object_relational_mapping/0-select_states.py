b89bd1d2ebce788cfb4388f2e623b14cad070f2d
grem62 <gremontmatheo22@gmail.com>
2023-07-31 13:55:54 +0200

    correction bug

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -11,13 +11,13 @@ if __name__ == '__main__':
         passwd=sys.argv[2],
         db=sys.argv[3])
 
-cur = db.cursor()
+    cur = db.cursor()
 
-cur.execute("SELECT * FROM states ORDER BY id ASC;")
+    cur.execute("SELECT * FROM states ORDER BY id ASC;")
 
-states = cur.fetchall()
-for state in states:
-    print(state)
+    states = cur.fetchall()
+    for state in states:
+        print(state)
 
 cur.close()
 db.close()
