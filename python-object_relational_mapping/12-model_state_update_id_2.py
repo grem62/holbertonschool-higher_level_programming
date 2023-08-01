9b30ea9e082636339efb9b7ff438e20cd4406629
grem62 <gremontmatheo22@gmail.com>
2023-08-01 14:54:42 +0200

    change the states 2

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Lists all State objects from the database hbtn_0e_6_usa
+"""
+
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+
+    # Replace 'localhost' with your MySQL server hostname if needed
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state_to_change = session.query(State).filter_by(id=2).first()
+
+    # Change the name of the State to "New Mexico"
+    if state_to_change:
+        state_to_change.name = 'New Mexico'
+
+        # Commit the changes to the database
+        session.commit()
+
+    # Close the session
+    session.close()
