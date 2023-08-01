e71af3cd71aedbe738e068d9ac19c84ac391177a
grem62 <gremontmatheo22@gmail.com>
2023-08-01 10:51:14 +0200

    add the class state

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,15 +1,12 @@
 #!/user/bin/pyton3
-
+"""class state"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-import MySQLdb
 
 Base = declarative_base()
 
 
 class State(Base):
-    __tbalename__ = 'some_table'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(128))
-    db = MySQLdb.connect(
-        host="localhost", port=3306,)
+    __tbalename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
