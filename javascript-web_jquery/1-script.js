9cfc7511f29b57d5a25e10aa905cb311a47007ae
grem62 <gremontmatheo22@gmail.com>
2023-09-18 11:01:29 +0200

    add task 1

---
+++ javascript-web_jquery/1-script.js
@@ -0,0 +1,5 @@
+//  Sélectionnez l'élément avec l'ID "header" en utilisant jQuery
+const update = $('header');
+
+//  Modifiez la couleur du texte en utilisant la méthode .css() de jQuery
+update.css('color', '#FF0000');
