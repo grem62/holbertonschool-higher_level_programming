194eb988062d15f4e508f730d940879ac86d7e5c
grem62 <gremontmatheo22@gmail.com>
2023-09-18 11:15:24 +0200

    add task 3

---
+++ javascript-web_jquery/3-script.js
@@ -0,0 +1,5 @@
+// Add a click event listener to the <div> with ID "red_header"
+$('#red_header').click(function () {
+// Add the class "red" to the <header> element
+  $('header').addClass('red');
+});
