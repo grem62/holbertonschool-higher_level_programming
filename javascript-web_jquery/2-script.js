ae98a24219309def54ac3a8a4229541a124fa0ad
grem62 <gremontmatheo22@gmail.com>
2023-09-18 11:09:27 +0200

    add task 2

---
+++ javascript-web_jquery/2-script.js
@@ -0,0 +1,5 @@
+// Add a click event listener to the <div> with ID "red_header"
+$('#red_header').click(function () {
+  // Select the <header> element and change its text color to red
+  $('header').css('color', '#FF0000');
+});
